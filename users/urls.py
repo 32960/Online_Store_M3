6d4f66e477b841c728d44d89d564aaa427a99b89
Andrei Zoteev <andrej-sam@rambler.ru>
2026-06-29 19:55:50 +0300

    feat: - delete "forgot password"

--- users/urls.py
+++ users/urls.py
@@ -13,8 +13,6 @@ from users.views import (
     OrderDetailView,
     OrderCancelView,
 )
-from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
 
 app_name = 'users'
 
@@ -23,35 +21,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
-
-    # Password Reset
-    path('forgot-password/',
-         auth_views.PasswordResetView.as_view(
-             template_name='users/forgot_password.html',
-             email_template_name='users/password_reset_email.html',
-             subject_template_name='users/password_reset_subject.txt',
-             success_url=reverse_lazy('users:password_reset_done'),
-         ),
-         name='forgot-password'),
-
-    path('forgot-password/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html',
-         ),
-         name='password_reset_done'),
-
-    path('reset-password/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/reset_password.html',
-             success_url=reverse_lazy('users:password_reset_complete'),
-         ),
-         name='password_reset_confirm'),
-
-    path('reset-password/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='users/reset_password_complete.html',
-         ),
-         name='password_reset_complete'),
 
     # Personal Account
     path('account/', AccountView.as_view(), name='account'),
